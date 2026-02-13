20cc9c8493c4de8cab5ebacab6f2a6d43794a536
EF <vcbbgh@gmx.com>
2026-02-13 23:48:35 +0000

    auto-update: all files

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@
  */
 (function () {
   var SUPPORTED_LANGUAGES = [
-    'en', 'cs', 'da', 'de', 'el', 'es', 'fr', 'hr', 'hu', 'it', 'lt', 'mt', 'nl', 'pl', 'pt', 'ro', 'sk', 'sl', 'sv', 'uk'
+    'en', 'cs', 'da', 'de', 'el', 'es', 'fr', 'hr', 'hu', 'it', 'lt', 'mt',
+    'nl', 'pl', 'pt', 'ro', 'sk', 'sl', 'sv', 'uk', 'bg'
   ];
 
   function getPreferredLanguage() {
